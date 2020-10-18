aac4cbbe39d93b42c9c62965af1d4c9fe6df9675
Veeqtor <nwokeochavictor@gmail.com>
2020-10-18 12:00:03 +0100

    Update site data

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,23 +5,26 @@ import data from "../data/siteData"
 class Skills extends PureComponent {
   render() {
     return (
-      <div id='skills'>
-        <h3 className="wow fadeIn"
-            data-wow-duration="2s"
-            data-wow-offset="100"
-        >skills</h3>
-        <div className="soft">
+      <div id="skills">
+        <h3 className="wow fadeIn" data-wow-duration="2s" data-wow-offset="100">
+          skills
+        </h3>
+        {/* <div className="soft">
           {data.softSkills.map((skill, index) =>
             <SkillBar key={`skill.subject` + index} subject={skill.subject}
                       mark={`${skill.mark}%`}
                       fullMark={skill.fullMark}/>)}
-        </div>
+        </div> */}
 
         <div className="tech">
-          {data.technicalSkills.map((skill, index) =>
-            <SkillBar key={`skill.subject` + index} subject={skill.subject}
-                      mark={`${skill.mark}%`}
-                      fullMark={skill.fullMark}/>)}
+          {data.technicalSkills.map((skill, index) => (
+            <SkillBar
+              key={`skill.subject` + index}
+              subject={skill.subject}
+              mark={`${skill.mark}%`}
+              fullMark={skill.fullMark}
+            />
+          ))}
         </div>
       </div>
     )
--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,23 +1,29 @@
 const data = {
   brand: "victor nwokeocha",
-  navLinks: [{
-    name: "About me",
-    path: "#about",
-  }, {
-    name: "Skills",
-    path: "#skills",
-  }, {
-    name: "My Story",
-    path: "#experience",
-  }, {
-    name: "Contact",
-    path: "#contact",
-  }],
+  navLinks: [
+    {
+      name: "About me",
+      path: "#about",
+    },
+    {
+      name: "Skills",
+      path: "#skills",
+    },
+    {
+      name: "My Story",
+      path: "#experience",
+    },
+    {
+      name: "Contact",
+      path: "#contact",
+    },
+  ],
   userInfo: {
-    name: "Victor nwokeocha",
-    title: "Software engineer",
+    name: "Victor Nwokeocha",
+    title: "Software Engineer",
     location: "Lagos, Nigeria",
-    description: "I am a technology enthusiast, passionate about learning " +
+    description:
+      "I am a technology enthusiast, passionate about learning " +
       "the trends the tech world is taking so that I can build better, " +
       "fast and efficient tools to help solve problems now, or in the future.",
     gitResume: "https://resume.github.io/?veeqtor",
@@ -31,86 +37,109 @@ const data = {
   },
   technicalSkills: [
     {
-      subject: "Python/Flask", mark: 93, fullMark: 100,
+      subject: "Python/Flask",
+      mark: 86,
+      fullMark: 100,
     },
     {
-      subject: "Python/Django", mark: 82, fullMark: 100,
+      subject: "Python/Django",
+      mark: 87,
+      fullMark: 100,
     },
     {
-      subject: "Javascript/Reactjs", mark: 78, fullMark: 100,
+      subject: "Javascript/Reactjs",
+      mark: 82,
+      fullMark: 100,
     },
     {
-      subject: "Javascript/Angular", mark: 80, fullMark: 100,
+      subject: "Javascript/Angular",
+      mark: 68,
+      fullMark: 100,
     },
     {
-      subject: "Html5/Css3", mark: 87, fullMark: 100,
+      subject: "Html5/Css3",
+      mark: 87,
+      fullMark: 100,
     },
   ],
-  softSkills: [
-    {
-      subject: "Communication", mark: 89, fullMark: 100,
-    },
-    {
-      subject: "Professionalism", mark: 82, fullMark: 100,
-    },
-    {
-      subject: "Integration", mark: 78, fullMark: 100,
-    },
-    {
-      subject: "Quality", mark: 80, fullMark: 100,
-    },
-  ],
+  // softSkills: [
+  //   {
+  //     subject: "Communication",
+  //     mark: 89,
+  //     fullMark: 100,
+  //   },
+  //   {
+  //     subject: "Professionalism",
+  //     mark: 82,
+  //     fullMark: 100,
+  //   },
+  //   {
+  //     subject: "Integration",
+  //     mark: 78,
+  //     fullMark: 100,
+  //   },
+  //   {
+  //     subject: "Quality",
+  //     mark: 80,
+  //     fullMark: 100,
+  //   },
+  // ],
   careerHistory: [
     {
-      year: "April 2015 - October 2015",
-      title: "Technical Intern",
-      company: "National Center for Energy Research and Development",
-      location: "University of Nigeria, Nsukka",
+      year: "Oct 2016 - July 2018",
+      title: "Software Developer",
+      company: "Freelance",
+      location: "Lagos, Nigeria",
       descriptions: [
-        "Installed and maintained solar panels and inverters.",
-        "Researched on Renewable energy",
+        "Built a poultry management application.",
+        "Built and deployed a flight reservation API",
+        "Tools & Technologies: Python - Django; // REST // PostgreSQL // Docker // CI/CD.",
       ],
-    }, {
-      year: "June 2017 - April 2018",
-      title: "Technical Assistant",
-      company: "Ministry of Works, Housing and Transport",
-      location: "Tarauni, Kano state",
-      descriptions: [
-        "Maintained and repaired street lights and electrical machines",
-        "Installed Street lights and electrical machines.",
-        "Designed electrical wiring and fittings.",
-      ],
-    }, {
+    },
+    {
       year: "July 2018 - Present",
       title: "Software Engineer",
       company: "Andela",
-      location: "Lagos state",
+      location: "Lagos, Nigeria",
       descriptions: [
-        "Contribute to the development and maintenance of in-house tools using Python/Flask, Javascript/React/NodeJs, PostgreSQL",
-        "I build and maintain products for our partners with Python/Django, Javascript/Angular/Nodejs, Docker, AWS, MySQL",
-        "For self-development purposes, I built a flight booking API that enables a user to make flight reservations, " +
-        "and book flight tickets, using Django and Django REST framework.",
-        "Continuous self-development.",
+        "Was involved in the development of an asset management system that is currently being used across 3 locations at Andela to manage assets and space allocations, increasing space utilization by 30%.",
+        "Mentored and facilitated young developers seeking a new career path as a software engineer at Andela.",
+        "Tools & Technologies: JavaScript - React, Redux; Python - Flask; // REST // PostgreSQL, Redis // Docker // CI/CD.",
       ],
-    }, {
+    },
+    {
+      year: "May 2020 - May 2020",
+      title: "Frontend Software Engineer",
+      company: "Prunedge",
+      location: "Lagos, Nigeria",
+      descriptions: [
+        "Built the frontend dashboard of a COVID-19 notification system which is being used by the state government to notify individuals of their COVID-19 test status.s",
+        "Tools & Technologies: React, Redux, Azure, Apexchart, Ant design.",
+      ],
+    },
+    {
       year: "April 2019 - Present",
       title: "Software Engineer",
       company: "Voyage Control",
       location: "London, United Kingdom",
       descriptions: [
-        "I design and implement front-end and back-end logics to interpret the business needs of the platform.",
-        "I designed and wrote an efficient, testable code with low-latency and high availability to integrate with an external API to meet the business needs and satisfy our users.",
-        "I currently work with a team of Software Engineers to improve and maintain optimal performance on the voyage control platform using Python/Django, Javascript/Angular/Nodejs, Docker/AWS.",
+        "Collaborated with the engineering teams to optimize the performance of Voyage Control’s Slot duration service by 60% using eager fetching techniques and generators functions.",
+        "Built the Milk Run delivery feature which is currently helping our users cut the pickup and drop-off time by making a single booking for both pickups and drop-offs using one vehicle/truck only.",
+        "Reduced the amount of spamming/abusive activities on the voyage control platform by 90% using an advanced risk analysis engine and adaptive challenges to keep automated software from abusing the platform.",
+        "Ensured that our clients have full access to their deliveries and schedules on both the platform and a third party platform by integrating their API with the voyage control platform.",
+        "Enhanced the user experience of the admin page on our platform by redesigning and implementing a modern aesthetic page where they can perform admin actions with less hassle.",
+        "I work across different teams (Frontend, Backend, and DevOps), writing efficient, testable code with low-latency and high availability to improve and maintain optimal performance on all the services used by the platform and to also meet the business needs and increase user satisfaction.",
+        "Tools & Technologies: JavaScript - AngularJs, Anguar2+; Python - Django; // REST // MySQL, Redis, RabbitMQ, Solr // Docker, AWS // CI/CD.",
       ],
     },
   ],
   academicHistory: [
-    {
-      year: "2005 - 2011",
-      school: "Command Day Secondary School, Enugu",
-      certification: "Secondary Education - O'Level (WASSCE/NECO)",
-      descriptions: null,
-    },
+    // {
+    //   year: "2005 - 2011",
+    //   school: "Command Day Secondary School, Enugu",
+    //   certification: "Secondary Education - O'Level (WASSCE/NECO)",
+    //   descriptions: null,
+    // },
     {
       year: "2011 - 2016",
       school: "University of Nigeria, Nsukka",
